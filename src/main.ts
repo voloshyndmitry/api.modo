c2154bece33523ee2f8ca671484e34f7a26636ff
Dmytro Voloshyn <Dmytro.Voloshyn@wolterskluwer.com>
2023-06-18 09:02:35 -0600

    turn on cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5019;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(PORT);
 }
 bootstrap();

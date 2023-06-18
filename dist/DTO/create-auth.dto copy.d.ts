c4762f9a8fee86037a8b3214285072bb4a9c0d47
Dmytro Voloshyn <Dmytro.Voloshyn@wolterskluwer.com>
2023-06-18 08:57:03 -0600

    add custom port number

--- dist/DTO/create-auth.dto copy.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class CreateAuthDto {
-    readonly login: string;
-    readonly pass: string;
-}
--- dist/main.js
+++ dist/main.js
@@ -2,9 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const PORT = process.env.PORT || 3000;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map

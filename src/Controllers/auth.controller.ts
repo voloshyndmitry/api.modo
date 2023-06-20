7a7606a2bd951a6a8991b5abf61bea420ecad18b
Dmytro Voloshyn <Dmytro.Voloshyn@wolterskluwer.com>
2023-06-19 22:33:10 -0600

    add login2

--- src/Controllers/auth.controller.ts
+++ src/Controllers/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from "@nestjs/common";
 import { AuthService } from "../Services/auth.service";
 
 @Controller("auth")
@@ -9,5 +17,11 @@ export class AuthController {
   @Post("login")
   signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.email, signInDto.password);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Get("login")
+  signIn2(@Param("login") email: string, @Param("pass") password: string) {
+    return this.authService.signIn(email, password);
   }
 }

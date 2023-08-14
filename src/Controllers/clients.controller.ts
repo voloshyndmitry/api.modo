40a07904ed74924fd9b5f7b4f11b15a4f3655a95
Voloshyn Dmytro <voloshyndmytro@Voloshyns-MacBook-Pro.local>
2023-08-13 23:06:59 -0600

    feature/status-canculation-clean-up

--- src/Controllers/clients.controller.ts
+++ src/Controllers/clients.controller.ts
@@ -57,10 +57,10 @@ export class ClientsController {
     return this.ClientsService.updateValue(CreateClientsDto, req.user);
   }
 
-  // @UseGuards(AuthGuard)
+  @UseGuards(AuthGuard)
   @Get()
   async findAll(@Request() req: CustomRequest): Promise<ClientsDataClass[]> {
-    return this.ClientsService.findAll(req.user || { sub: 1});
+    return this.ClientsService.findAll(req.user);
   }
 
   @UseGuards(AuthGuard)

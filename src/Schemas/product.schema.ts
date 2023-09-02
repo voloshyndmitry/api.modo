912b8c65e9608700dd05be70e66df9ffc9afea0f
Voloshyn Dmytro <voloshyndmytro@Voloshyns-MacBook-Pro.local>
2023-09-01 19:36:14 -0600

    fix/update-products-schema

--- src/Schemas/product.schema.ts
+++ src/Schemas/product.schema.ts
@@ -18,6 +18,16 @@ export class Offer {
 }
 
 export const OfferSchema = SchemaFactory.createForClass(Offer);
+@Schema()
+export class Color {
+  @Prop()
+  label: string;
+
+  @Prop()
+  value: string;
+}
+
+export const ColorSchema = SchemaFactory.createForClass(Color);
 
 @Schema({ collection: "products" })
 export class ProductDataClass {
@@ -42,11 +52,8 @@ export class ProductDataClass {
   @Prop({ type: String })
   brand?: string;
 
-  @Prop({ type: String })
-  color: {
-    label: string;
-    value: string;
-  };
+  @Prop({ type: ColorSchema })
+  color: Color;
 
   @Prop({ type: String })
   salePrice: number

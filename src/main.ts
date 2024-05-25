e3bae577ae32884697d52a42486f75f670cd7652
olena <voloshynajelena@gmail.com>
2024-05-24 19:53:55 -0600

    (config) swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+      .setTitle('Modo Api')
+      .setVersion('1.0')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(PORT);
 }
 bootstrap();

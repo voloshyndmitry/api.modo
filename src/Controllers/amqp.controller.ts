caef539c7bc8be7a503d8c686f1c75c86f0e6aec
olena <voloshynajelena@gmail.com>
2024-05-24 20:11:12 -0600

    (config) amqp

---
+++ src/Controllers/amqp.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Inject } from '@nestjs/common';
+import { ClientProxy, EventPattern } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+    constructor(@Inject('RABBITMQ_SERVICE') private readonly client: ClientProxy) {}
+
+    @Get('send')
+    async sendMessage() {
+        const message = { text: 'Hello from RabbitMQ Microservice' };
+        return this.client.emit('message_printed', message);
+    }
+
+    @EventPattern('message_printed')
+    handleMessagePrinted(data: Record<string, unknown>) {
+        console.log('Received message:', data);
+    }
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,32 @@ import { AppService } from "./Services/app.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from "./Modules/auth.module";
 import { UsersModule } from "./Modules/users.module";
-import { ClientsModule } from "./Modules/clients.module";
 import { EventsModule } from "./Modules/events.module";
 import { VisitsModule } from "./Modules/visits.module";
 import { PaymentsModule } from "./Modules/payments.module";
 import { MembershipsModule } from "./Modules/memberships.module";
 import { AnalyticsModule } from "./Modules/analytics.module";
 import { ProductsModule } from "./Modules/products.module";
+import { Transport, ClientsModule } from '@nestjs/microservices';
 
 require("dotenv").config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGO_KEY),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.CLOUDAMQP_URL || 'amqp://localhost:5672'],
+          queue: 'main_queue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
     AuthModule,
     UsersModule,
     ClientsModule,
